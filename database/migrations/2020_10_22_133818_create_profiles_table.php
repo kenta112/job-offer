854ded4c0a12f1b63b98f7ce1c26cdc1aea9c15b
kenta112 <mpkenta@gmail.com>
2020-10-22 14:05:46 +0000

    make:controller profile

---
+++ database/migrations/2020_10_22_133818_create_profiles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('employment')->nullable();
+            $table->string('language')->nullable();
+            $table->string('location')->nullable();
+            $table->string('appeal')->nullable();
+            $table->string('future')->nullable();
+            $table->string('icon')->nullable();
+            $table->string('image')->nullable();
+            $table->string('birthday')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
