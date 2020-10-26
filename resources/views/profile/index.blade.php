e4debd3bd7ce3ba3e7cafe2c4958f7ab7faca6e9
naoki7024 <naoki102470@gmail.com>
2020-10-26 22:40:26 +0000

    auth guest処理追加

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -11,37 +11,37 @@
                 <a href="https://sa-i.jp/">Sai Tech</a>
             </h1>
             <nav class="pc-nav">
-                <ul>　
-                　　<li><a href="{{route('register')}}">会員登録</a></li>
+                <ul>　                    
+        <!-- // ユーザーは認証済み -->
+                    @auth
                     <li><a href="{{route('login')}}">企業ログイン</a></li>
                     <li><a href="{{route('login')}}">生徒ログイン</a></li>
                     <li><a href="{{route('mypage.index')}}">マイページ</a></li>
+                    @endauth
+
+        <!-- // ユーザーは認証されていない -->
+                    @guest
+                　　<li><a href="{{route('register')}}">会員登録</a></li>
+                    @endguest
                 </ul>
             </nav>
         </header>
+        
         <div class="Navigation">
-            <p></p>　
+        <h1>aaaaaaaaaaaaaaaaaaaaaaaa</h1>
         </div>
-        <div class="content">　
-            <div class="card">
-                <div class="card__imgframe" src="">写真入る</div>
-                <div class="card__textbox">
-                    <div class="card__titletext">
-                        ホゲホゲホゲほげ
-                    </div>
-                    <div class="card__overviewtext">
-                        ホゲホゲホゲ
-                    </div>
-                </div>
-            </div>
-            <div class="card">
-                <div class="card__imgframe" src="">写真入る</div>
-                <div class="card__textbox">
-                    <div class="card__titletext">
-                        ホゲホゲホゲほげ
-                    </div>
-                    <div class="card__overviewtext">
-                        ホゲホゲホゲ
+        <!-- 生徒一覧表記 -->
+        <div class="content">　 
+            <div class="container"> 
+                <div class="card">
+                    <div class="card__imgframe" src="">写真入る</div>
+                    <div class="card__textbox">
+                        <div class="card__titletext">
+                            ホゲホゲホゲほげ
+                        </div>
+                        <div class="card__overviewtext">
+                            ホゲホゲホゲ
+                        </div>
                     </div>
                 </div>
             </div>
