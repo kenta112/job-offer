120030adb3828a5c8fccb2bd453e3a49c9933a55
kenta112 <mpkenta@gmail.com>
2020-10-26 11:46:55 +0000

    profile/show.blade.php mod

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -3,8 +3,6 @@
 <h3>氏名：{{ $user->name }}さん</h3>
 <p>メールアドレス：{{ $user->email }}</p>
 
-
-
 <p>生年月日：{{ $profile->birthday }}</p>
 <p>使用言語：{{ $profile->language }}</p>
 <p>希望する雇用形態：{{ $profile->employment }}</p>
@@ -15,4 +13,5 @@
 <p>アピールポイント：{{ $profile->appeal }}</p>
 <p>将来像：{{ $profile->future }}</p>
 
+<!-- <a href="{{ route('') }}"></a> -->
 
