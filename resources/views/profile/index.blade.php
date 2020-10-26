d614e3f90bf504f5f4bd69af25ae65e826184f6b
naoki7024 <naoki102470@gmail.com>
2020-10-26 09:10:36 +0000

    top　画面編集

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -1,2 +1,25 @@
-top
-<a href="{{route('mypage.index')}}">マイページ</a>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <header>
+        <h1>Sai Tech</h1>
+        <a href="{{route('mypage.index')}}">マイページ</a>
+    </header>
+    <main>
+        <p>〜生徒一覧〜</p>
+    </main>    
+    <footer>
+    
+
+
+    </footer>
+</body>
+</html>
+
