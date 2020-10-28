8299aaf135367510115cc57c3145ab8a463b700a
naoki7024 <naoki102470@gmail.com>
2020-10-28 11:53:58 +0000

    top 編集

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -12,29 +12,28 @@
             </h1>
             <nav class="pc-nav">
                 <ul>　                    
-        <!-- // ユーザーは認証済み -->
+        <!-- ユーザーは認証済み -->
                     @auth
                     <li><a href="{{route('login')}}">企業ログイン</a></li>
                     <li><a href="{{route('login')}}">生徒ログイン</a></li>
                     <li><a href="{{route('mypage.index')}}">マイページ</a></li>
                     @endauth
-
-        <!-- // ユーザーは認証されていない -->
+        <!-- ユーザーは認証されていない -->
                     @guest
                 　　<li><a href="{{route('register')}}">会員登録</a></li>
                     @endguest
                 </ul>
             </nav>
         </header>
-        
-        <div class="Navigation">
-        <h1>aaaaaaaaaaaaaaaaaaaaaaaa</h1>
+        <div class="navigation">
+            <p>グローバルナビゲーション</p>
         </div>
+
         <!-- 生徒一覧表記 -->
         <div class="content">　 
             <div class="container"> 
                 <div class="card">
-                    <div class="card__imgframe" src="">写真入る</div>
+                    <div class="card__imgframe" src="">写真</div>
                     <div class="card__textbox">
                         <div class="card__titletext">
                             ホゲホゲホゲほげ
@@ -51,4 +50,4 @@
             <p>Saitech</p>
         </footer>
         </body>
-<html>
+<html> 
